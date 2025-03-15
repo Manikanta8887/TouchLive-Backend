3c4c60767b3605c914edf7eba5ff51d1968ccb71
Manikanta8887 <swamimanikanta66@gmail.com>
2025-03-15 18:41:12 +0530

    api updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app = express();
 // cors are used to allow resources from other ports
 app.use(cors({
-  origin: "https://full-stack-project-mani.vercel.app/",
+  origin: "https://full-stack-project-mani.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));

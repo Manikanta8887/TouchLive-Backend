8430e0247bd504d813d6fa8b466b19205b4a693b
Manikanta8887 <swamimanikanta66@gmail.com>
2025-03-15 15:51:50 +0530

    Save

--- Controllers/bioUpdate.js
+++ Controllers/bioUpdate.js
@@ -12,7 +12,7 @@ const updateUserProfile = async (req, res) => {
     if (!updatedUser) return res.status(404).json({ message: "User not found" });
     res.json(updatedUser);
   } catch (error) {
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Server Error" });
   }
 };
 

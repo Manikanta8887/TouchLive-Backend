0fb44c988e8abcfbaebd42010218e423cb92f212
Manikanta8887 <swamimanikanta66@gmail.com>
2025-04-14 22:14:51 +0530

    Backend code

--- Models/Stream.js
+++ Models/Stream.js
@@ -1,3 +1,28 @@
+// import mongoose from "mongoose";
+
+// const StreamSchema = new mongoose.Schema(
+//   {
+//     streamerId: { type: String, required: true },
+//     streamTitle: { type: String, required: true },
+//     startTime: { type: Date, required: true },
+//     endTime: { type: Date },
+//     viewers: { type: Number, default: 0 },
+//     isFullscreen: { type: Boolean, default: false },
+//     chatMessages: [
+//       {
+//         sender: String,
+//         message: String,
+//         timestamp: { type: Date, default: Date.now },
+//       },
+//     ],
+//   },
+//   { timestamps: true }
+// );
+
+// const Stream = mongoose.model("Stream", StreamSchema);
+// export default Stream;
+
+// Models/Stream.js
 import mongoose from "mongoose";
 
 const StreamSchema = new mongoose.Schema(
--- Routes/streamRoutes.js
+++ Routes/streamRoutes.js
@@ -1,16 +1,28 @@
+// import express from "express";
+// import { getActiveStreams, getStreamChat, getPastStreams } from "../Controllers/streamController.js";
+
+// const router = express.Router();
+
+
+// router.get("/active", getActiveStreams);
+
+
+// router.get("/chat/:streamId", getStreamChat);
+
+
+// router.get("/past/:userId", getPastStreams);
+
+// export default router;
+
+
+// Routes/streamRoutes.js
 import express from "express";
 import { getActiveStreams, getStreamChat, getPastStreams } from "../Controllers/streamController.js";
 
 const router = express.Router();
 
-
 router.get("/active", getActiveStreams);
-
-
 router.get("/chat/:streamId", getStreamChat);
-
-
 router.get("/past/:userId", getPastStreams);
 
 export default router;
-
